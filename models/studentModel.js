3a9228703fffdc001295e01c9062f0bf363b173d
andre90messana@gmail.com <andreamessana@MBP-di-Andrea.homenet.telecomitalia.it>
2023-06-26 16:33:23 +0200

    implementazione ulteriore di studentModel

--- models/studentModel.js
+++ models/studentModel.js
@@ -26,38 +26,148 @@ const studentSchema = new Schema({
         required: true,
         max: 15
     },
-    school_subjects: [
-        {
-            storia: {
-                orale: [Number],
-                scritto: [Number]
-            },
-            filosofia: {
-                orale: [Number],
-                scritto: [Number]
-            },
-            italiano: {
-                orale: [Number],
-                scritto: [Number]
-            },
-            matematica: {
-                orale: [Number],
-                scritto: [Number]
-            },
-            scienze: {
-                orale: [Number],
-                scritto: [Number]
-            },
-            educazione_fisica: {
-                orale: [Number],
-                scritto: [Number]
-            },
-            fisica: {
-                orale: [Number],
-                scritto: [Number]
-            },
-        }
-    ]
+    school_subjects: {
+        primo_quadrimestre :[
+            {
+                storia: {
+                    orale: [Number],
+                    scritto: [Number],
+                    media_orale: Number,
+                    media_scritto: Number,
+                    media_fine_primo_quadrimestre: Number
+                },
+                filosofia: {
+                    orale: [Number],
+                    scritto: [Number],
+                    media_orale: Number,
+                    media_scritto: Number,
+                    media_fine_primo_quadrimestre: Number
+                },
+                italiano: {
+                    orale: [Number],
+                    scritto: [Number],
+                    media_orale: Number,
+                    media_scritto: Number,
+                    media_fine_primo_quadrimestre: Number
+                },
+                matematica: {
+                    orale: [Number],
+                    scritto: [Number],
+                    media_orale: Number,
+                    media_scritto: Number,
+                    media_fine_primo_quadrimestre: Number
+                },
+                scienze: {
+                    orale: [Number],
+                    scritto: [Number],
+                    media_orale: Number,
+                    media_scritto: Number,
+                    media_fine_primo_quadrimestre: Number
+                },
+                educazione_fisica: {
+                    orale: [Number],
+                    scritto: [Number],
+                    media_orale: Number,
+                    media_scritto: Number,
+                    media_fine_primo_quadrimestre: Number
+                },
+                fisica: {
+                    orale: [Number],
+                    scritto: [Number],
+                    media_orale: Number,
+                    media_scritto: Number,
+                    media_fine_primo_quadrimestre: Number
+                },
+                comportamento: {
+                    voto: Number
+                }
+            }
+        ],
+        secondo_quadrimestre :[
+            {
+                storia: {
+                    orale: [Number],
+                    scritto: [Number],
+                    media_orale: Number,
+                    media_scritto: Number,
+                    media_fine_secondo_quadrimestre: Number
+                },
+                filosofia: {
+                    orale: [Number],
+                    scritto: [Number],
+                    media_orale: Number,
+                    media_scritto: Number,
+                    media_fine_secondo_quadrimestre: Number
+                },
+                italiano: {
+                    orale: [Number],
+                    scritto: [Number],
+                    media_orale: Number,
+                    media_scritto: Number,
+                    media_fine_secondo_quadrimestre: Number
+                },
+                matematica: {
+                    orale: [Number],
+                    scritto: [Number],
+                    media_orale: Number,
+                    media_scritto: Number,
+                    media_fine_secondo_quadrimestre: Number
+                },
+                scienze: {
+                    orale: [Number],
+                    scritto: [Number],
+                    media_orale: Number,
+                    media_scritto: Number,
+                    media_fine_secondo_quadrimestre: Number
+                },
+                educazione_fisica: {
+                    orale: [Number],
+                    scritto: [Number],
+                    media_orale: Number,
+                    media_scritto: Number,
+                    media_fine_secondo_quadrimestre: Number
+                },
+                fisica: {
+                    orale: [Number],
+                    scritto: [Number],
+                    media_orale: Number,
+                    media_scritto: Number,
+                    media_fine_secondo_quadrimestre: Number
+                },
+                comportamento: {
+                    voto: Number
+                }
+            }
+        ],
+        Media_voti_finale :[
+            {
+                storia: {
+                    media: Number
+                },
+                filosofia: {
+                    media: Number
+                },
+                italiano: {
+                    media: Number
+                },
+                matematica: {
+                    media: Number
+                },
+                scienze: {
+                    media: Number
+                },
+                educazione_fisica: {
+                    media: Number
+                },
+                fisica: {
+                    media: Number
+                },
+                comportamento: {
+                    voto: Number
+                }
+            }
+        ]
+    } 
 },
     {
         timestamps: true,

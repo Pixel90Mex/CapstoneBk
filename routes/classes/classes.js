2567056dd681d6f2f171dbd9ac3196764df32313
andre90messana@gmail.com <andreamessana@MacBook-Pro-di-Andrea.local>
2023-07-04 20:44:52 +0200

    fetures per esame

--- routes/classes/classes.js
+++ routes/classes/classes.js
@@ -1,39 +1,45 @@
 import { Router } from "express";
-import bcrypt from "bcrypt";
 import classModel from "../../models/classes/classModel.js";
+import studentModel from "../../models/students/studentModel.js";
+import teacherModel from "../../models/teachers/teacherModel.js";
 
 const router = Router();
 
 //GET
-router.get("/classes", async(req, res) => {
-    const { page = 1, pageSize =3} = req.params;
+router.get("/classes", async (req, res) => {
+    const { page = 1, pageSize = 13 } = req.query;
 
     try {
-        const classes = await classModel.find()
-        .populate("class.students")
-        .populate("class.teachers")
-        .limit(pageSize)
-        .skip((page -1) * pageSize);
+        const classes = await classModel
+            .find()
+            .populate("class.students")
+            .populate("class.teachers")
+            .limit(pageSize)
+            .skip((page - 1) * pageSize);
+        
+        const totalClasses = await classModel.count();
 
         res.status(200).send({
             currentPage: +page,
+            totalClasses: totalClasses,
             classes,
             statusCode: 200
         })
     } catch (error) {
         res.status(500).send({
             message: "Errore interno del server",
+            error: error.message,
             statusCode: 500
         })
     }
 });
 //POST --> tengo in considerazione che insegnanti e studenti siano già stati creati
-router.post("/classes", async(req, res)=> {
-
+router.post("/classes", async (req, res) => {
+    const { section, teachers, students} = req.body;
     //quando monto frontend metto controllo per la section 
     //ciclo con condizionale per la section su array di studenti che arriva nella request + return 
     const classToInsert = await classModel({
-        class:{
+        class: {
             section: req.body.class.section,
             teachers: req.body.class.teachers,
             students: req.body.class.students
@@ -44,7 +50,7 @@ router.post("/classes", async(req, res)=> {
         const sectionExist = await classModel.findOne({
             section: req.body.section
         });
-        if(sectionExist) {
+        if (sectionExist) {
             return res.status(409).send({
                 message: "Sezione già registrata",
                 statusCode: 409
@@ -58,10 +64,15 @@ router.post("/classes", async(req, res)=> {
         })
     } catch (error) {
         res.status(500).send({
-            message:"Errore interno del server:" + error,//concateno errore per vedere di cosa si tratta
+            message: "Errore interno del server",
+            error: error.message,
             statusCode: 500
         })
     }
 });
+
+//PATCH PER MODIFICARE LISTA STUDENTI E DOCENTI (AGGIUNGERE O TOGLIERE)
+
+//DELETE PER CANCELLARE CLASSE, TOGLIERE STUDENTE O DOCENTE
 
 export default router;
